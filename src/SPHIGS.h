059e50c554af2de7e37b12e2b070d4fd35811dc7
Michael Rockhold <michael@appel-rockhold.org>
2022-05-09 20:22:00 -0700

    xcodify: WIP

---
+++ src/SPHIGS.h
@@ -0,0 +1,18 @@
+//
+//  SPHIGS.h
+//  SPHIGS
+//
+//  Created by Michael Rockhold on 4/30/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPHIGS.
+FOUNDATION_EXPORT double SPHIGSVersionNumber;
+
+//! Project version string for SPHIGS.
+FOUNDATION_EXPORT const unsigned char SPHIGSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPHIGS/PublicHeader.h>
+
+
